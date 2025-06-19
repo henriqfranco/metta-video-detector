6d9f733bf3b13e33ed4d5a6fe3bf071aae4d5333
Henrique Franco <henriqfvr@gmail.com>
2025-06-19 17:37:49 -0300

    Elementos da pasta output_results deletados visando rodar o sistema utilizando o modelo yolo11x para maior precisão na detecção; Função detect refatorada: for loop desnecessário removido ao perceber que, por estar passando uma imagem/frame por vez, não há necessidade de loopar pelos resultados (garantia de que existe apenas um resultado);

--- src/detector.py
+++ src/detector.py
@@ -3,25 +3,23 @@ import cv2
 
 class PeopleDetector:
     def __init__(self):
-        self.model = YOLO("yolo11n.pt")
+        self.model = YOLO("yolo11x.pt")
     
     def detect(self, frame):
         results = self.model.predict(frame, verbose=False)
         # print(f"Results: {results}")
+        result = results[0]
+        # print(f"Result: {result}")
+        boxes = result.boxes
+        # print(f"Boxes: {boxes}")
         people_boxes = []
 
-        for result in results:
-            # print(f"Result: {result}")
-            boxes = result.boxes
-            # print(f"Boxes: {boxes}")
-            for box in boxes:
-                # print(f"Box: {box}")
-                cls = int(box.cls[0])
-                # print(f"Cls: {cls}")
-                if cls == 0:
-                    x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    people_boxes.append((x1, y1, x2, y2))
-                    # print(f"People boxes: {people_boxes}")
+        for box in boxes:
+            # print(f"Box: {box}")
+            cls = int(box.cls[0])
+            if cls == 0:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                people_boxes.append((x1, y1, x2, y2))
 
         return people_boxes
 
@@ -32,7 +30,6 @@ if __name__ == "__main__":
     detector = PeopleDetector()
     
     boxes = detector.detect(frame)
-    # print(boxes)
     
     for (x1, y1, x2, y2) in boxes:
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
