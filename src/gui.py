1c7c545f1db542dbf269f6d95293e7ecd64288ff
Henrique Franco <henriqfvr@gmail.com>
2025-06-21 00:27:33 -0300

    Alteração no figsize de ambos os gráficos visando melhorar a visibilidade; Implementação de slider seletor de nivel de detalhes dos gráficos.

--- src/gui.py
+++ src/gui.py
@@ -4,6 +4,8 @@ import json
 import shutil
 import matplotlib.pyplot as plt
 from video_processor import VideoProcessor
+from scipy.interpolate import make_interp_spline
+import numpy as np
 
 st.set_page_config(page_title="Contador de Pessoas", layout="centered")
 
@@ -39,6 +41,16 @@ if os.path.exists(video_file):
     st.subheader("Vídeo Processado")
     st.video(video_file)
 
+if os.path.exists(output_path):
+    plot_detail = st.slider(
+            "Nível de Detalhe dos Gráficos",
+            min_value=10,
+            max_value=100,
+            value=50,
+            format="%i%%"
+        )
+    plot_detail /= 100
+
 history_file = os.path.join(output_path, "history.json")
 if os.path.exists(history_file):
     with open(history_file) as f:
@@ -46,10 +58,14 @@ if os.path.exists(history_file):
 
     frame_ids = [entry["id"] for entry in data]
     counts = [entry["count"] for entry in data]
+    
+    frame_ids_spaced = np.linspace(min(frame_ids), max(frame_ids), int(max(frame_ids)*plot_detail))
+    spl = make_interp_spline(frame_ids, counts, k=0)
+    counts_smooth = spl(frame_ids_spaced)
 
     st.subheader("Gráfico de contagem de pessoas por frame")
-    fig, ax = plt.subplots()
-    ax.plot(frame_ids, counts, label="Pessoas por frame")
+    fig, ax = plt.subplots(figsize=(18,9))
+    ax.plot(frame_ids_spaced, counts_smooth, label="Pessoas por frame")
     ax.set_xlabel("Frame")
     ax.set_ylabel("Número de pessoas")
     ax.grid(True)
@@ -65,10 +81,14 @@ if os.path.exists(alerts_file):
 
     frame_ids = [entry["id"] for entry in alerts]
     counts = [entry["count"] for entry in alerts]
+    
+    frame_ids_spaced = np.linspace(min(frame_ids), max(frame_ids), int(max(frame_ids)*plot_detail))
+    spl = make_interp_spline(frame_ids, counts, k=0)
+    counts_smooth = spl(frame_ids_spaced)
 
     st.subheader(f"Gráfico de contagem de pessoas por frame (com mais de {threshold_used} pessoas)")
-    fig, ax = plt.subplots()
-    ax.plot(frame_ids, counts, label="Pessoas por frame")
+    fig, ax = plt.subplots(figsize=(18,9))
+    ax.plot(frame_ids_spaced, counts_smooth, label="Pessoas por frame")
     ax.set_xlabel("Frame")
     ax.set_ylabel("Número de pessoas")
     ax.grid(True)
